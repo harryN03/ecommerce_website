ec6238e5ef9ffb2fa84cb4d28ee6c3be19b53928
Harry Nguyen <123419956+harryN03@users.noreply.github.com>
2023-09-13 22:53:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Router>
             <Navbar />
             <Routes>
-              <Route path="/" element={<Shop />}></Route>
+              <Route path="/ecommerce_website/" element={<Shop />}></Route>
               <Route path="cart" element={<Cart />}></Route>
               <Route path="checkout" element={<Checkout />} />
             </Routes>
